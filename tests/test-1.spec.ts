ab3d43d2569f98222b1d1490948b31e09a04d0b5
joaki <joakim_holmstrom8@hotmail.com>
2026-05-17 10:12:27 +0200

    Updated test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -2,5 +2,22 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
-  await page.getByRole('button', { name: 'Count is' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('admin');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('password');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Add new task...' }).click();
+  await page.getByRole('textbox', { name: 'Add new task...' }).fill('');
+  await page.getByRole('textbox', { name: 'Add new task...' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Add new task...' }).fill('T');
+  await page.getByRole('textbox', { name: 'Add new task...' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Add new task...' }).fill('Testing t');
+  await page.getByRole('textbox', { name: 'Add new task...' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Add new task...' }).fill('Testing T');
+  await page.getByRole('textbox', { name: 'Add new task...' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Add new task...' }).fill('Testing Things');
+  await page.getByRole('button', { name: 'Add' }).click();
+  await page.getByRole('checkbox').check();
+  await page.getByRole('checkbox').check();
 });
