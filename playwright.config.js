5d35f111df0bd94c5f01e27e2f2ac62099048040
joaki <joakim_holmstrom8@hotmail.com>
2026-05-18 23:43:28 +0200

    Small Update to Playwright Config

--- playwright.config.js
+++ playwright.config.js
@@ -35,8 +35,8 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
+   {
+      name: 'chrome',
       use: { ...devices['Desktop Chrome'] },
     },
 
