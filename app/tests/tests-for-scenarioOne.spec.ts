74e63869e1ee3232d8147c90e41f3fdfac722b00
joaki <joakim_holmstrom8@hotmail.com>
2026-05-26 16:14:23 +0200

    Workflow Change

--- app/tests/tests-for-scenarioOne.spec.ts
+++ app/tests/tests-for-scenarioOne.spec.ts
@@ -1,41 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('scenario one - version one', async ({ page }) => {
-await page.goto('/');
-  await page.waitForTimeout(1000);
-  // Weak selectors
-  await page.locator('input').nth(0).fill('admin');
-  await page.waitForTimeout(500);
-  await page.locator('input').nth(1).fill('password');
-  await page.waitForTimeout(500);
-  // Weak button selector
-  await page.locator('button').nth(0).click();
-  // Hardcoded waits
-  await page.waitForTimeout(2000);
-  // BAD: generic selectors
-  await page.locator('input').nth(0).fill('Make food');
-  await page.locator('button').nth(1).click();
-  await page.waitForTimeout(500);
-  await page.locator('input').nth(0).fill('Clean Room');
-  await page.locator('button').nth(1).click();
-  await page.waitForTimeout(500);
-  await page.locator('input').nth(0).fill('Walk the dog');
-  await page.locator('button').nth(1).click();
-  // Fragile checkbox selector
-  await page.locator('input[type="checkbox"]').nth(0).check();
-  // Clicking buttons by index = very flaky
-  await page.locator('button').nth(2).click();
-
-  await page.locator('button').nth(3).click();
-  await page.locator('button').nth(4).click();
-
-  // Delete task using index
-  await page.locator('button').nth(6).click();
-});
-
 test('scenario one - version two', async ({ page }) => {
   await page.goto('/');
-
   // Login
   await page.getByRole('textbox', {
     name: 'Username'
